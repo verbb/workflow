a23e0f4060bb733c60cfb5799c51163b01a5b191
Josh Crawford <joshua@sgroup.com.au>
2016-06-26 03:10:22 +1000

    fix that enabled sections work, and templating fixes

--- workflow/services/WorkflowService.php
+++ workflow/services/WorkflowService.php
@@ -29,38 +29,44 @@ class WorkflowService extends BaseApplicationComponent
 
         // Only show the sidebar submission button for editors
         if ($user->isInGroup($editorGroup)) {
-            craft()->templates->hook('cp.entries.edit.right-pane', function(&$context) {
-                if (!$context['entry']->id) {
-                    return;
-                }
-
-                // See if there's an existing submission
-                $submission = craft()->workflow_submissions->getByElementId($context['entry']->id);
-
-                return craft()->templates->render('workflow/_includes/editor-pane', array(
-                    'context' => $context,
-                    'submission' => $submission,
-                ));
-            });
+            $this->_renderEntrySidebarPanel('editor-pane');
         }
 
         // Show another information panel for publishers (if there's submission info)
         if ($user->isInGroup($publisherGroup)) {
-            craft()->templates->hook('cp.entries.edit.right-pane', function(&$context) {
-                if (!$context['entry']->id) {
-                    return;
-                }
-
-                // See if there's an existing submission
-                $submission = craft()->workflow_submissions->getByElementId($context['entry']->id);
-
-                return craft()->templates->render('workflow/_includes/publisher-pane', array(
-                    'context' => $context,
-                    'submission' => $submission,
-                ));
-            });
+            $this->_renderEntrySidebarPanel('publisher-pane');
         }
     }
-    
+
+
+
+    // Private Methods
+    // =========================================================================
+
+    private function _renderEntrySidebarPanel($template)
+    {
+        $settings = $this->getSettings();
+
+        craft()->templates->hook('cp.entries.edit.right-pane', function(&$context) use ($settings, $template) {
+            if (!$context['entry']->id) {
+                return;
+            }
+
+            // Make sure workflow is enabled for this section - or all section
+            if ($settings->enabledSections != '*') {
+                if (!in_array($context['entry']->sectionId, $settings->enabledSections)) {
+                    return;
+                }
+            }
+
+            // See if there's an existing submission
+            $submission = craft()->workflow_submissions->getByElementId($context['entry']->id);
+
+            return craft()->templates->render('workflow/_includes/' . $template, array(
+                'context' => $context,
+                'submission' => $submission,
+            ));
+        });
+    }
 
 }
