75432ed1ea37837f15e1ce9aecfb078b40ee41ab
Josh Crawford <joshua@sgroup.com.au>
2019-03-06 14:50:39 +1100

    Allow non-admins to save plugin settings

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -42,4 +42,38 @@ class BaseController extends Controller
             'publishers' => $publishers,
         ]);
     }
+
+    public function actionSavePluginSettings()
+    {
+        $currentUser = Craft::$app->getUser()->getIdentity();
+
+        if (!$currentUser->can('workflow:settings')) {
+            throw new ForbiddenHttpException('User is not permitted to perform this action.');
+        }
+
+        $this->requirePostRequest();
+
+        $pluginHandle = Craft::$app->getRequest()->getRequiredBodyParam('pluginHandle');
+        $settings = Craft::$app->getRequest()->getBodyParam('settings', []);
+        $plugin = Craft::$app->getPlugins()->getPlugin($pluginHandle);
+
+        if ($plugin === null) {
+            throw new NotFoundHttpException('Plugin not found');
+        }
+
+        if (!Craft::$app->getPlugins()->savePluginSettings($plugin, $settings)) {
+            Craft::$app->getSession()->setError(Craft::t('app', "Couldn't save plugin settings."));
+
+            // Send the plugin back to the template
+            Craft::$app->getUrlManager()->setRouteParams([
+                'plugin' => $plugin,
+            ]);
+
+            return null;
+        }
+
+        Craft::$app->getSession()->setNotice(Craft::t('app', 'Plugin settings saved.'));
+
+        return $this->redirectToPostedUrl();
+    }
 }
