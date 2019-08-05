55454616a5fa1a424e50dca6e2b503dc193b9775
Josh Crawford <joshua@sgroup.com.au>
2019-08-06 09:18:37 +1000

    Revert button behaviour back to pre-3.2

--- src/resources/src/js/workflow.js
+++ src/resources/src/js/workflow.js
@@ -11,31 +11,31 @@ if (typeof Craft.Workflow === typeof undefined) {
 
 (function($) {
 
-$('[data-workflow-btn]').on('click', function(e) {
-    e.preventDefault();
+// $('[data-workflow-btn]').on('click', function(e) {
+//     e.preventDefault();
 
-    // Prevent browser prompt when trying to submit the form - thinking its changed
-    $(window).off('beforeunload');
+//     // Prevent browser prompt when trying to submit the form - thinking its changed
+//     $(window).off('beforeunload');
 
-    var $form = $(this).parents('form');
-    var $btn = $(e.currentTarget);
+//     var $form = $(this).parents('form');
+//     var $btn = $(e.currentTarget);
 
-    if ($btn.attr('data-action')) {
-        $('<input type="hidden" name="action"/>').val($btn.attr('data-action')).appendTo($form);
-    }
+//     if ($btn.attr('data-action')) {
+//         $('<input type="hidden" name="action"/>').val($btn.attr('data-action')).appendTo($form);
+//     }
 
-    if ($btn.attr('data-redirect')) {
-        $('<input type="hidden" name="redirect"/>').val($btn.attr('data-redirect')).appendTo($form);
-    }
+//     if ($btn.attr('data-redirect')) {
+//         $('<input type="hidden" name="redirect"/>').val($btn.attr('data-redirect')).appendTo($form);
+//     }
 
-    if ($btn.attr('data-param')) {
-        $('<input type="hidden"/>').attr({ name: $btn.attr('data-param'), value: $btn.attr('data-value') }).appendTo($form);
-    }
+//     if ($btn.attr('data-param')) {
+//         $('<input type="hidden"/>').attr({ name: $btn.attr('data-param'), value: $btn.attr('data-value') }).appendTo($form);
+//     }
 
-    $form.trigger({
-        type: 'submit',
-        customTrigger: true,
-    });
-});
+//     $form.trigger({
+//         type: 'submit',
+//         customTrigger: true,
+//     });
+// });
 
 })(jQuery);
