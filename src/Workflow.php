1088f56a92912b97992d297c90287a7d0909fc41
Josh Crawford <joshua@sgroup.com.au>
2023-11-22 08:31:59 +1100

    The Workflow control panel menu item now will only show if the user has “Overview” permission

--- src/Workflow.php
+++ src/Workflow.php
@@ -41,7 +41,6 @@ class Workflow extends Plugin
     // Properties
     // =========================================================================
 
-    public bool $hasCpSection = true;
     public bool $hasCpSettings = true;
     public string $schemaVersion = '2.5.0';
     public string $minVersionRequired = '1.7.0';
@@ -73,6 +72,13 @@ class Workflow extends Plugin
         if (Craft::$app->getRequest()->getIsCpRequest()) {
             $this->_registerCpRoutes();
             $this->_registerWidgets();
+
+            // Only show the menu item if user has permission to overview
+            if ($currentUser = Craft::$app->getUser()->getIdentity()) {
+                if ($currentUser->can('workflow-overview')) {
+                    $this->hasCpSection = true;
+                }
+            }
         }
 
         if (Craft::$app->getRequest()->getIsConsoleRequest()) {
